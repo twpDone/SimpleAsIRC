a95cf968345c98e6840f6a6ea277d2ed7627c184
TwpDone <root@localhost.localdomain>
2014-04-11 14:09:58 +0200

    Add comments for documentation

--- Action.py
+++ Action.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from Message import Message
 import subprocess
 
+##
+# Class to store one Action
 class Action:
+
+    ##
+    # Constructor.
+    # @param self Self.
+    # @param triggerText Text to trigger the Action.
+    # @param functionToExecute Execute the Action
+    # @note If Text matches the Action should be performed.
     def __init__(self,triggerText,functionToExecute):
+
+        ## @var m_triggerText 
+        # Text to trigger the Action
         self.m_triggerText=triggerText;
+
+        ## @var m_functionToExecute
+        # Fonction a executer
         self.m_functionToExecute=functionToExecute;
+
+    ##
+    # Perform the stored function.
+    # @param Core Core object.
+    # @param Message Message object.
     def doAction(self,Core,Message):
         try:
+            # Try to execute the function
             self.m_functionToExecute(Core,Message);
         except Exception as e:
             print("Erreur lors du declenchement de l'action : ")
