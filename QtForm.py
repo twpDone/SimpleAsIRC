12785ab72f5ace527abbc01f6a49b4e29adf8147
zero <zero@Famine>
2014-06-16 23:35:21 +0200

    Test code Fonctional, todo : Thread management

--- QtForm.py
+++ QtForm.py
@@ -12,17 +12,17 @@ class Form(QDialog):
         layout.addWidget(self.browser)
         layout.addWidget(self.lineedit)
         self.setLayout(layout)
-        self.lineeedit.setFocus()
-        self.connect(self.lineeedit, SIGNAL("returnPressed()"), self.sendMessage)
+        self.lineedit.setFocus()
+        self.connect(self.lineedit, SIGNAL("returnPressed()"), self.sendMessage)
         self.setWindowTitle("SimpleAsIrc")
         self.textToSend=""
     def sendMessage(self):
-        text=self.lineeedit.text()
+        text=self.lineedit.text()
         self.browser.append(str(text))
         self.textToSend=str(text)
     def display(self,message):
         text="["+message.getDest()+"] "+message.getName().replace(":","")+": "+message.getText()
-        self.browser.append(str(text))
+        self.browser.append(text)
     def getInput(self):
         try:
             # try to get raw input
